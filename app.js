092ced8c7c41e1e5ac0e899a0e6c87276cb10ecb
Damir Zeric <damir.zeric.mes@gmail.com>
2023-03-07 21:20:19 +0100

    GET, POST, DELETE

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.set('view engine', 'ejs');
 
 // 3rd party middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Mongoose and mongo sandbox routes
@@ -75,14 +76,35 @@ app.get('/about', (req, res) => {
 // Blog routes
 app.get('/blogs', (req, res) => {
     Blog.find().sort({ createdAt: -1 }) // -1 - descending
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result });
-        }).catch((err) => console.log(err));
+        .then((result) => res.render('index', { title: 'All Blogs', blogs: result }))
+        .catch((err) => console.log(err));
+});
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    const blog = Blog.findById(id)
+        .then((result) => res.render('details', { title: 'Blog Details', blog: result }))
+        .catch((err) => console.log(err));
 });
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new Blog' });
 });
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result) => res.redirect('/blogs'))
+        .catch((err) => console.log(err));
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then((result) => res.json({ redirect: '/blogs' }))
+        .catch((err) => console.log(err));
+});
+
 
 // Redirect
 
