a67e27119dd7d682b10bf04b128ee5a328eca6ba
Damir Zeric <damir.zeric.mes@gmail.com>
2023-03-11 23:15:34 +0100

    MVC

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
+
 const app = express();
 
 // Connect to MongoDB
@@ -20,49 +21,10 @@ app.set('view engine', 'ejs');
 // app.set('views', 'path');
 
 
-// Middleware examples
-// app.use((req, res, next) => {
-//     console.log('New request made:');
-//     console.log('Host: ', req.hostname);
-//     console.log('Path: ', req.path);
-//     console.log('Method: ', req.method);
-//     next(); // Move to next handler
-// });
-
-// app.use((req, res, next) => {
-//     console.log('In the next middleware');
-//     next();
-// });
-
 // 3rd party middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
-// Mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'New Blog 2',
-        snippet: 'About my new blog.',
-        body: 'More about my new blog.'
-    });
-
-    blog.save()
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-});
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-});
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('640639fac73c1036265acc7e')
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-});
 
 app.get('/', (req, res) => {
     // res.render('index', { title: 'Home', blogs });
@@ -74,37 +36,7 @@ app.get('/about', (req, res) => {
 });
 
 // Blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 }) // -1 - descending
-        .then((result) => res.render('index', { title: 'All Blogs', blogs: result }))
-        .catch((err) => console.log(err));
-});
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    const blog = Blog.findById(id)
-        .then((result) => res.render('details', { title: 'Blog Details', blog: result }))
-        .catch((err) => console.log(err));
-});
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog' });
-});
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result) => res.redirect('/blogs'))
-        .catch((err) => console.log(err));
-});
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then((result) => res.json({ redirect: '/blogs' }))
-        .catch((err) => console.log(err));
-});
-
+app.use('/blogs', blogRoutes);
 
 // Redirect
 
--- server.js
+++ server.js
@@ -47,3 +47,42 @@ const server = http.createServer((req, res) => {
 server.listen(3000, 'localhost', () => {
     console.log('Listening for request on port 3000...');
 });
+
+// Middleware examples
+// app.use((req, res, next) => {
+//     console.log('New request made:');
+//     console.log('Host: ', req.hostname);
+//     console.log('Path: ', req.path);
+//     console.log('Method: ', req.method);
+//     next(); // Move to next handler
+// });
+
+// app.use((req, res, next) => {
+//     console.log('In the next middleware');
+//     next();
+// });
+
+// Mongoose and mongo sandbox routes
+// app.get('/add-blog', (req, res) => {
+//     const blog = new Blog({
+//         title: 'New Blog 2',
+//         snippet: 'About my new blog.',
+//         body: 'More about my new blog.'
+//     });
+
+//     blog.save()
+//         .then((result) => res.send(result))
+//         .catch((err) => console.log(err));
+// });
+
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//         .then((result) => res.send(result))
+//         .catch((err) => console.log(err));
+// });
+
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById('640639fac73c1036265acc7e')
+//         .then((result) => res.send(result))
+//         .catch((err) => console.log(err));
+// });
